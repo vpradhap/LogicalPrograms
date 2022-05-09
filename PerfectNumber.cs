ea2b61dcde2829b6553b7c4f67c3d30a10776828
vpradhap <pradhapvenkat@gmail.com>
2022-05-09 18:02:02 +0530

    Added : Perfect Number Checking Program

---
+++ PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public static void Perfect()
+        {
+            int temp = 0;
+            Console.Write("\nEnter The Number to Check : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num/2; i++)
+            {
+                
+                if (num % i == 0)
+                {
+                    temp += i;
+                    Console.Write(i+" ");
+                }
+            }
+            Console.Write( "= "+temp);
+            
+            if (temp == num)
+            {
+                Console.WriteLine("\n\t"+num+" is a Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine("\n\t"+num + " is Not a Perfect Number");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
 ﻿using LogicalPrograms;
 
-Console.WriteLine("Logical Programs");
-FibonacciSeries.Fibonacci();
+Console.WriteLine("Logical Programs\n");
+Console.WriteLine("Pick The Program You Want to Execute\n");
+Console.WriteLine("1 - Fibonacci Series Program");
+Console.WriteLine("2 - Perfect Number Program");
+Console.Write("\nEnter your Pick : ");
+int pick = Convert.ToInt32(Console.ReadLine());
+
+switch (pick)
+{
+    case 1:
+        FibonacciSeries.Fibonacci();
+        break;
+    case 2:
+        PerfectNumber.Perfect();
+        break;
+}
