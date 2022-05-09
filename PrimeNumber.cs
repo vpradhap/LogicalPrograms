615261fcfe648a8568c992ed186775546cd921f3
vpradhap <pradhapvenkat@gmail.com>
2022-05-09 19:03:23 +0530

    Added : Prime Number Checking Program

---
+++ PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public static void Prime()
+        {
+            int count = 0;
+            Console.Write("\nEnter a Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                    Console.WriteLine("\n\t" + num + " is Not a Prime Number");
+                    break;
+                }    
+            }
+            if(count == 0)
+            {
+                Console.WriteLine("\n\t" + num + " is a Prime Number");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Logical Programs\n");
 Console.WriteLine("Pick The Program You Want to Execute\n");
 Console.WriteLine("1 - Fibonacci Series Program");
 Console.WriteLine("2 - Perfect Number Program");
+Console.WriteLine("3 - Prime Number Program");
 Console.Write("\nEnter your Pick : ");
 int pick = Convert.ToInt32(Console.ReadLine());
 
@@ -14,5 +15,11 @@ switch (pick)
         break;
     case 2:
         PerfectNumber.Perfect();
+        break;
+    case 3:
+        PrimeNumber.Prime();
+        break;
+    default:
+        Console.WriteLine("Invalid Pick");
         break;
 }
