4df93123d05a02c1b5222a894d9f26329910dd49
vpradhap <pradhapvenkat@gmail.com>
2022-05-10 13:39:40 +0530

    Added : StopWatch Program

--- Program.cs
+++ Program.cs
@@ -2,28 +2,40 @@
 
 Console.WriteLine("Logical Programs\n");
 Console.WriteLine("Pick The Program You Want to Execute\n");
+Console.WriteLine("0 - Enter zero To Exit ");
 Console.WriteLine("1 - Fibonacci Series Program");
 Console.WriteLine("2 - Perfect Number Program");
 Console.WriteLine("3 - Prime Number Program");
 Console.WriteLine("4 - Reverse a Number Program");
-Console.Write("\nEnter your Pick : ");
-int pick = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("5 - StopWatch Program");
 
-switch (pick)
+int flag = 0;
+while(flag == 0)
 {
-    case 1:
-        FibonacciSeries.Fibonacci();
-        break;
-    case 2:
-        PerfectNumber.Perfect();
-        break;
-    case 3:
-        PrimeNumber.Prime();
-        break;
-    case 4:
-        ReverseANumber.ReverseNumber();
-        break;
-    default:
-        Console.WriteLine("Invalid Pick");
-        break;
+    Console.Write("\nEnter your Pick : ");
+    int pick = Convert.ToInt32(Console.ReadLine());
+    switch (pick)
+    {
+        case 0:
+            flag = 1;
+            break;
+        case 1:
+            FibonacciSeries.Fibonacci();
+            break;
+        case 2:
+            PerfectNumber.Perfect();
+            break;
+        case 3:
+            PrimeNumber.Prime();
+            break;
+        case 4:
+            ReverseANumber.ReverseNumber();
+            break;
+        case 5:
+            StopWatch.Stop();
+            break;
+        default:
+            Console.WriteLine("Invalid Pick");
+            break;
+    }
 }
---
+++ StopWatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class StopWatch
+    {
+        public static void Stop()
+        {
+            Console.Write("\nTo Start the Stopwatch Enter S or s : ");
+            char start = Convert.ToChar(Console.ReadLine());
+            Stopwatch sw = Stopwatch.StartNew();
+            if (start == 's' || start == 'S')
+            {
+                sw.Start();
+                Console.Write("\nTo Stop the Stopwatch Enter Q or q : ");
+                char end = Convert.ToChar(Console.ReadLine());
+                if (end == 'q' || end == 'Q')
+                {
+                    sw.Stop();
+                    Console.WriteLine("\n\tThe Time Elapsed : " + sw.Elapsed.TotalSeconds+" seconds");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+        }
+    }
+}
